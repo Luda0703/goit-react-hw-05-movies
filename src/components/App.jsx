ef0a00a0ff529936c3baa89bf0b05e46d3a1c463
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2023-06-12 21:37:22 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
+import Movies from "pages/Movies";
+import { Route, Routes } from "react-router-dom";
+import Home from '../pages/Home';
+import Layout from "./Layout/Layout";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Routes>
+        <Route path='/' element={<Layout/>}>
+          <Route index element={<Home/>}/>
+          <Route path='movies' element={<Movies/>}>
+            <Route path='/:movieId' element={<div> MovieDetails</div>}>
+              <Route path='/cast' element={<div> Cast</div>}/>
+              <Route path='/reviews' element={<div> Reviews</div>}/>
+            </Route>
+          </Route>
+        </Route>
+      </Routes>
     </div>
   );
 };
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+// import { useEffect } from "react";
+
+
+const Home = () => {
+    // useEffect(() => {
+
+    // })
+    return (
+        <>
+        <div>Home</div>
+        </>
+    )
+}
+
+export default Home;
+
+// key ab2cf7e1a236e63f2cc161486f07b55f
